04547f4b1f669d86f8d8eb026d432b977c2dc91c
odeyalo <theprogrammerinfuture@gmail.com>
2025-10-16 17:31:00 +0300

    Initialize the project

---
+++ src/main/java/com/odeyalo/sonata/playlist/metagen/PlaylistMetaGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.odeyalo.sonata.playlist.metagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistMetaGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaylistMetaGenerationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odeyalo/sonata/playlist/metagen/PlaylistMetaGenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeyalo.sonata.playlist.metagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistMetaGenerationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
